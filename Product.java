80d06f8b96f73ce91b61998b032d39549f86e5ad
Eoin Gohery <eoingohery@gmail.com>
2018-11-20 11:47:36 +0000

    k

---
+++ Product.java
@@ -0,0 +1,33 @@
+/**
+   A product in a vending machine.
+*/
+public class Product
+{
+   private String description;
+   private double price;
+
+   /**
+      Constructs a Product object
+      @param aDescription the description of the product
+      @param aPrice the price of the product
+   */
+   public Product(String aDescription, double aPrice)
+   {
+      description = aDescription;
+      price = aPrice;
+   }
+
+   public String getDescription() {
+     return description;
+   }
+
+   public double getPrice() {
+     return price;
+   }
+
+   public String toString() {
+     return ("Name:\t" + description + "\nPrice: \t" + price);
+   }
+
+	//ADD REMAINING CODE HERE
+}
