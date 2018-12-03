1272199a50410ed37d0fea636bd5b1fd680993b8
Eoin Gohery <eoingohery@gmail.com>
2018-12-03 14:27:29 +0000

    machine changed to manager in cli

--- CLI.java
+++ CLI.java
@@ -5,19 +5,18 @@ public class CLI
   public static void start()
 		 {
 	try{
-				{ 
-					VendingManager machine = new VendingManager();
+				{
+					VendingManager manager = new VendingManager();
 					VendingMachineMenu menu = new VendingMachineMenu();
 					menu.run(machine);
-					
+
 				}
 		}
 	catch (Exception e)
 			 {
-				 
+
 			 }
-			 
+
 		 }
-		 
+
 		 }
-		 
