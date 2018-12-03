433e26f4b6009e60964eb641e738fca63d302af9
Eoin Gohery <eoingohery@gmail.com>
2018-12-03 17:31:50 +0000

    vc vbcv

--- GUI.java
+++ GUI.java
@@ -6,31 +6,31 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 public class GUI extends Application {
 
+    Scene productScene;
+		Scene coinScene;
+
    public static void main(String[] args) {
       launch(args);
    }
 
     @Override
     public void start(Stage primaryStage) {
-      window = primaryStage;
-      window.setTitle("Vending Machine");
+      primaryStage.setTitle("Vending Machine");
       //fail = new Button("Click Me");
 			Button EnterCoins = new Button("Enter Coins");
 			Button ViewProducts = new Button("View Products");
       //fail.setOnAction(e -> AlertBox.display("Error", "Incorrect code."));
-			EnterCoins.setOnAction(e -> window.setScene(coinScene));
-			EnterCoins.setOnAction(e -> window.setScene(coinScene));
+			EnterCoins.setOnAction(e -> primaryStage.setScene(coinScene));
+			EnterCoins.setOnAction(e -> primaryStage.setScene(coinScene));
       StackPane layout = new StackPane();
       layout.getChildren().addAll(EnterCoins, ViewProducts);
       Scene scene = new Scene(layout, 300, 250);
-      window.setScene(scene);
-      window.show();
-
+      primaryStage.setScene(scene);
+      primaryStage.show();
     }
 		//scene for adding coins
 		//Layout 1 - children laid out in vertical column
-		public void AddCoinG() {
-			Scene coinScene;
+
 			Button fiftyCent = new Button("50 cent");
 			Button twentyCent = new Button("20 cent");
 			Button euro = new Button("1 euro");
@@ -39,14 +39,9 @@ public class GUI extends Application {
       VBox layout1 = new VBox(20);
       layout1.getChildren().addAll(label1,fiftyCent,twentyCent,euro,twoEuro);
       coinScene = new Scene(layout1, 200, 200);
-		}
 
-		public void productList() {
-			Scene productScene;
 			ChoiceBox<Product> choiceBox = new ChoiceBox<>();
 			choiceBox.getItems().addAll(machine.getProductTypes());
-			VBox layout1 = new VBox(20);
-      layout1.getChildren().addAll(label1,fiftyCent,twentyCent,euro,twoEuro);
       productScene = new Scene(layout1, 200, 200);
-		}
+
 }
