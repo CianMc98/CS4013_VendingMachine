1ab8d062dc0c585ace67bc804c4607527c166a1f
Eoin Gohery <eoingohery@gmail.com>
2018-12-03 18:03:28 +0000

    updated

--- GUI.java
+++ GUI.java
@@ -9,38 +9,20 @@ import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 
-public class GUI extends Application {
+public class GUI extends Menu {
 
-    Stage window;
+    Stage primaryStage;
     Button fail;
 
-    public GUI(String[] args) {
-        launch(args);
+    public GUI() {
+      start(primaryStage);
     }
 
-    @Override
     public void start(Stage primaryStage) {
-      window = primaryStage;
-      window.setTitle("Vending Machine");
-      fail = new Button("Click Me");
-			Button EnterCoins = new Button("Enter Coins");
-			Button ViewProducts = new Button("View Products");
-      fail.setOnAction(e -> AlertBox.display("Error", "Incorrect code."));
-			EnterCoins.setOnAction(e -> window.setScene(coinScene));
-			EnterCoins.setOnAction(e -> window.setScene(coinScene));
-      StackPane layout = new StackPane();
-      layout.getChildren().addAll(EnterCoins, ViewProducts);
-      Scene scene = new Scene(layout, 300, 250);
-      window.setScene(scene);
-      window.show();
-
-
 		//scene for adding coins
-		//Layout 1 - children laid out in vertical column
-		AddCoinG() {
+		//Layout 1 - children laid out in vertical coloum
 			Scene coinScene;
 			Button fiftyCent = new Button("50 cent");
-			fiftyCent.setOnAction(e -> );
 			Button twentyCent = new Button("20 cent");
 			Button euro = new Button("1 euro");
 			Button twoEuro = new Button("2 euro");
@@ -48,16 +30,65 @@ public class GUI extends Application {
       VBox layout1 = new VBox(20);
       layout1.getChildren().addAll(label1,fiftyCent,twentyCent,euro,twoEuro);
       coinScene = new Scene(layout1, 200, 200);
-		}
-		}
 
-		public productList() {
-				Scene productScene;
-				ChoiceBox<LineItem> choiceBox = new ChoiceBox<>();
-				choiceBox.getItems().addAll(machine.getItemList());
+      primaryStage.setTitle("Vending Machine");
+      fail = new Button("Click Me");
+      Button EnterCoins = new Button("Enter Coins");
+      Button ViewProducts = new Button("View Products");
+      fail.setOnAction(e -> AlertBox.display("Error", "Incorrect code."));
+      EnterCoins.setOnAction(e -> primaryStage.setScene(coinScene));
+      EnterCoins.setOnAction(e -> primaryStage.setScene(coinScene));
+      StackPane layout = new StackPane();
+      layout.getChildren().addAll(EnterCoins, ViewProducts);
+      Scene scene = new Scene(layout, 300, 250);
+      primaryStage.setScene(scene);
+    }
 
-				VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1,fiftyCent,twentyCent,euro,twoEuro);
-        productScene = new Scene(layout1, 200, 200);
-		}
-}
+    public void run(VendingManager manager) {
+      boolean end = false;
+      while (!end) {
+        if (operator.getType() == 0) {
+          displayDefaultMenu(manager);
+        } else {
+          displayOperatorMenu(manager);
+        }
+      }
+    }
+
+    private void displayOperatorMenu(VendingManager manager) {
+        boolean more = true;
+        int operatorLevel = operator.getType();
+
+            if (operatorLevel == 1) {
+                System.out.println("S)how products  I)nsert coin  B)uy  A)dd Products  Q)uit");
+                primaryStage.show();
+            } else if (operatorLevel == 2) {
+                System.out.println("S)how products  I)nsert coin  B)uy  A)dd Products R)emove Coins  Q)uit");
+                primaryStage.show();
+            }
+
+        while (more) {
+
+                // Write lineitems and money to their respective files
+                manager.writeAllDataToFiles();
+
+            }
+        }
+
+    private void displayDefaultMenu(VendingManager manager) {
+        boolean more = true;
+        primaryStage.show();
+
+
+        while (more) {
+
+                more = false;
+
+                //more = false;
+                //end = true;
+                // Write lineitems and money to their respective files
+                manager.writeAllDataToFiles();
+
+            }
+        }
+    }
