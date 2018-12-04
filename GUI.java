ab27a6e6f5fe11fec464bdf003d2ca59abda0e3a
CianMc98 <38212861+CianMc98@users.noreply.github.com>
2018-12-04 19:25:46 +0000

    fix addproducts and input
    fix addproducts and input

--- GUI.java
+++ GUI.java
@@ -22,7 +22,7 @@ public class GUI extends Application {
 
  Stage primary;
  Button fail;
- Scene opScene, scene1, scene, coinScene, viewProducts, productsScene;
+ Scene opScene, scene1, scene, coinScene, viewProducts, productScene;
  VBox MainLayout;
  Button AddProducts = new Button("Add Product(s)");
  Button RemoveCoins = new Button("Remove Coins");
